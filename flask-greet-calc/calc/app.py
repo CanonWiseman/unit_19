e2c9567f227a3d18f20a09d0a6a426df8f71af4b
canonwiseman <wisemancanon@gmail.com>
2022-06-22 19:03:02 -0500

    added files for calc portion if the project

--- flask-greet-calc/calc/app.py
+++ flask-greet-calc/calc/app.py
@@ -1,1 +1,26 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+operators = {
+        "add": add,
+        "sub": sub,
+        "mult": mult,
+        "div": div,
+        }
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home_page():
+    return "<h1>Welcome to Calc</h1>"
+
+@app.route("/math/<operation>/")
+def math(operation):
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+
+    return f"The result is {operators[operation](a,b)}"
+
+
